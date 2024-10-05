eba091461b9c179a0adb119cbf9ed02d5981250e
Oscar Cuenca <ocuenca91@gmail.com>
2024-10-05 12:37:33 -0500

    switch

---
+++ proyectos-js/condicionales/scripts.js
@@ -0,0 +1,17 @@
+let valor;
+valor = parseInt(prompt("ingrese el valor"));
+
+switch (valor) {
+  case 1:
+    document.write("ingreso 1");
+    break;
+  case 2:
+    document.write("ingreso 2");
+    break;
+  case 3:
+    document.write("ingreso 3");
+    break;
+
+  default:
+    break;
+}
