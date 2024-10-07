f8a7ed94c4df301cfcdab11c98e161b47aed3290
Oscar Cuenca <ocuenca91@gmail.com>
2024-10-07 11:34:06 -0500

    Empezando funciones y el libro de hechizos

---
+++ encantamientos/libroencantamientos.js
@@ -0,0 +1,41 @@
+//Encantamientos
+//Encantamiento de reescritura, quita las letras que uno quiera y reescribe la palabra, en este caso quita la a, o mejor dicho, se la salta, podria utilizarla para quitar los espacios de una palabra, o seleccionar las letras que yo quiera
+
+function seleccionarCaracter(palabra, letra) {
+  let resultado = " ";
+  for (let key in palabra) {
+    if (palabra[key] == letra) {
+      continue;
+    } else {
+      resultado += palabra[key];
+    }
+  }
+  document.write(resultado);
+}
+//Ejemplo
+let letraa = "Hola a todos";
+seleccionarCaracter(letraa, "a");
+
+//------------------------------------------------------
+// saber cuantas vocales
+
+function seleccionadorVocales(palabra) {
+  let vocal = 0;
+  for (let key in palabra) {
+    if (
+      palabra[key] == "a" ||
+      palabra[key] == "e" ||
+      palabra[key] == "i" ||
+      palabra[key] == "o" ||
+      palabra[key] == "u"
+    ) {
+      vocal++;
+    }
+  }
+  document.write("Cantidad de vocales " + vocal);
+}
+
+//Ejemplo
+let cuantasVocales = "Cuantas vocales tienes";
+seleccionadorVocales(cuantasVocales);
+//-----------------------------------------------------------------------
--- proyectos-js/condicionales/scripts.js
+++ proyectos-js/condicionales/scripts.js
@@ -1,17 +1,10 @@
-let valor;
-valor = parseInt(prompt("ingrese el valor"));
-
-switch (valor) {
-  case 1:
-    document.write("ingreso 1");
-    break;
-  case 2:
-    document.write("ingreso 2");
-    break;
-  case 3:
-    document.write("ingreso 3");
-    break;
-
-  default:
-    break;
+let palabra = "JavaScript";
+let resultado = " ";
+for (let key in palabra) {
+  if (palabra[key] == "a") {
+    continue;
+  } else {
+    resultado += palabra[key];
+  }
 }
+document.write(resultado);
