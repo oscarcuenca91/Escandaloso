19c4194179056938e36d89c1e8dbaddbed6bfcb2
Oscar Cuenca <ocuenca91@gmail.com>
2024-10-09 09:11:35 -0500

    empezando poo

--- proyectos-js/condicionales/scripts.js
+++ proyectos-js/condicionales/scripts.js
@@ -1,5 +1,6 @@
 let palabra = "JavaScript";
 let resultado = " ";
+
 for (let key in palabra) {
   if (palabra[key] == "a") {
     continue;
@@ -8,3 +9,17 @@ for (let key in palabra) {
   }
 }
 document.write(resultado);
+document.write("<br>");
+
+function nombreSaludo() {
+  let nombre = prompt("Ingresa tu nombre");
+  document.write("Hola " + nombre);
+}
+
+document.write("<br>");
+
+function sumarNumeros(n1, n2) {
+  n1 = parseInt(prompt("Ingrese el número 1"));
+  n2 = parseInt(prompt("Ingese el número2"));
+  return n1 + n2;
+}
